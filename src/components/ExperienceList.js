4627e94e8698ee7484d0fcab5a5415a9b5cacacf
chrisAlide3 <christian.bracher@gmail.com>
2023-04-25 10:44:54 +0200

    Added comment

--- src/components/ExperienceList.js
+++ src/components/ExperienceList.js
@@ -1,6 +1,7 @@
 import experiences from "../allExperiences";
 import ExperienceCard from "./ExperienceCard";
 
+// Comment
 function ExperienceList() {
   experiences.map((experience) => {
     return <ExperienceCard name={experience.name} rating={experience.rating} />;
