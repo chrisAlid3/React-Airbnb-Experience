0da2f20bece4c9caafeed1d64d9a56004dc83910
chrisAlide3 <christian.bracher@gmail.com>
2023-05-09 09:50:50 +0200

    Redisigned Navbar with MUI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar.tsx";
 import ImageCollection from "./components/ImageCollection";
 import Subheader from "./components/Subheader";
 import ExperienceList from "./components/ExperienceList";
